a982dcfa4cbd6f6c497038f5c78ef951fae1b111
Jorge Bejar <jorge@wyeworks.com>
2017-04-04 16:58:27 -0300

    Use Devise helpers on Boards controller tests

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class BoardsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @board = boards(:one)
+    sign_in users(:one)
   end
 
   test "should get index" do
