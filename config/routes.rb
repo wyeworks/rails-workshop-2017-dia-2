4095cbd30afce0814677c1a4d35c7db218b2a08b
Jorge Bejar <jorge@wyeworks.com>
2017-04-05 17:00:05 -0300

    Define routes to add lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :boards
+
+  resources :boards do
+    resources :lists, only: [:new, :create]
+  end
 
   root 'boards#index'
 end
