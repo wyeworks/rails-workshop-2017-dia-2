09153c7805ee32dfc5d2040de4a11e3efab04b05
Emilio Cristalli <emilio@wyeworks.com>
2017-04-20 10:15:53 -0300

    Add route to create cards

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :boards do
-    resources :lists, only: [:new, :create]
+    resources :lists, only: [:new, :create], shallow: true do
+      resources :cards, only: :create
+    end
   end
 
   root 'boards#index'
