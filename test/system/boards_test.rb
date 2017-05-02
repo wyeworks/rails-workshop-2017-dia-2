231013ff90c5b00e3638aea2ff052cfa3ab52f96
Jorge Bejar <jorge@wyeworks.com>
2017-05-02 16:46:49 -0300

    Add a basic system test for the boards home page

--- test/system/boards_test.rb
+++ test/system/boards_test.rb
@@ -1,9 +1,19 @@
 require "application_system_test_case"
 
 class BoardsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit boards_url
-  #
-  #   assert_selector "h1", text: "Board"
-  # end
+
+  setup do
+    visit new_user_session_url
+
+    fill_in "Email", with: "one@example.com"
+    fill_in "Password", with: "MyPassword"
+
+    click_on "Log in"
+  end
+
+  test "visiting the index" do
+    visit boards_url
+
+    assert_selector "h1", text: "Boards"
+  end
 end
